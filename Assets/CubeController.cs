a4edc3e865461d7bb52b37f19bfa44cc7d50c905
yohan <unveryoh@students.zhaw.ch>
2023-04-20 14:19:24 +0200

    initial commit and add part1 of project

---
+++ Assets/CubeController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.IO;
+
+/*
+    Accelerates the cube to which it is attached, modelling an harmonic oscillator.
+    Writes the position, velocity and acceleration of the cube to a CSV file.
+    
+    Remark: For use in "Physics Engines" module at ZHAW, part of physics lab
+    Author: kemf
+    Version: 1.0
+*/
+public class CubeController : MonoBehaviour
+{
+    private Rigidbody rigidBody;
+
+    public int springConstant; // N/m
+
+    private float currentTimeStep; // s
+    
+    private List<List<float>> timeSeries;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        timeSeries = new List<List<float>>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // FixedUpdate can be called multiple times per frame
+    void FixedUpdate() {
+        float forceZ; // N
+
+        // Calculate spring force on body for x component of force vector
+        forceZ = -rigidBody.position.z * springConstant;
+        rigidBody.AddForce(new Vector3(0f, 0f, forceZ));
+
+        currentTimeStep += Time.deltaTime;
+        timeSeries.Add(new List<float>() {currentTimeStep, rigidBody.position.z, rigidBody.velocity.z, forceZ});
+    }
+
+    void OnApplicationQuit() {
+        WriteTimeSeriesToCSV();
+    }
+
+    void WriteTimeSeriesToCSV() {
+        using (var streamWriter = new StreamWriter("time_series.csv")) {
+            streamWriter.WriteLine("t,z(t),v(t),F(t) (added)");
+            
+            foreach (List<float> timeStep in timeSeries) {
+                streamWriter.WriteLine(string.Join(",", timeStep));
+                streamWriter.Flush();
+            }
+        }
+    }
+}
